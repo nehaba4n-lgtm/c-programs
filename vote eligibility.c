2ca59439d576508ddbf010e70d8433d063971485
nehaba4n-lgtm <nehaba4n@gmail.com>
2025-11-17 19:09:25 +0530

    Add files via upload

---
+++ vote eligibility.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age;
+        printf("Enter age: ");
+        scanf("%d",&age);
+        if(age >= 18){
+            printf("Can Vote");
+       } else
+            printf("Cannot Vote");
+        return 0;
+        }
