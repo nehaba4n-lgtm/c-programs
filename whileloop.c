5059a0bab8808a0405c0e4ead45597a0ba3e90a8
nehaba4n-lgtm <nehaba4n@gmail.com>
2025-11-17 11:30:22 +0530

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n, i =1,sum=0;
+    printf("Enter a positive number: ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum = sum +i;
+    i++;
+}
+printf("Sum =%d\n",sum);
+return 0;
+}
